4c6896f6a635870de0254f618003defc5665a8f8
dortega14 <dave.ort14@gmail.com>
2020-02-25 14:28:13 -0500

    controller layer (might change later)

--- batch-service/src/main/java/com/rms/controller/BatchController.java
+++ batch-service/src/main/java/com/rms/controller/BatchController.java
@@ -1,11 +1,18 @@
 package com.rms.controller;
 
+import com.rms.model.Batch;
 import com.rms.service.BatchService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/batch")
 public class BatchController{
 
     BatchService bs;
@@ -13,6 +20,26 @@ public class BatchController{
     @Autowired
     public void setBs(BatchService bs){
         this.bs = bs;
+    }
+
+    @GetMapping(value = "/allbatches")
+    public Iterable<Batch> readAllBatches() {
+        return bs.readAll();
+    }
+
+    @PostMapping(value = "/newbatch")
+    public Batch addNewbatch(Batch b) {
+        return bs.insert(b);
+    }
+
+    @PutMapping(value = "/updatebatch")
+    public Batch updateBatch(Batch b) {
+        return bs.update(b);
+    }
+
+    @DeleteMapping(value = "/deletebatch")
+    public void deleteBatch(Batch b) {
+        bs.delete(b);
     }
 
 }
--- batch-service/src/main/java/com/rms/controller/ResourceMetadataController.java
+++ batch-service/src/main/java/com/rms/controller/ResourceMetadataController.java
@@ -3,9 +3,11 @@ package com.rms.controller;
 import com.rms.service.ResourceMetadataService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class ResourceMetadataController {
 
     ResourceMetadataService rms;
